a8727f1109d820bf1571a54521f8285806d1d81d
Garima Vaishnav <garimavaishnav814@gmail.com>
2024-08-13 00:05:24 +0530

    nodemon package add

--- flashCardReact/src/main.jsx
+++ flashCardReact/src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import Login from './Componets/Ragister/Login.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Dashboard from './Componets/Dashboard/Dashboard.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />
       <Route path="adminLogin" element={<Login />} />
+      <Route path="dashboard" element={<Dashboard />} />
     </>
   )
 );
